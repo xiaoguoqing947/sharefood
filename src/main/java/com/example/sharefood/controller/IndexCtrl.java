5ca36940e03927552c2b1d85ce2b69034569d36b
Gumball <1159403411@qq.com>
2019-12-19 15:47:12 +0800

    美食界面导航条的点击

--- src/main/java/com/example/sharefood/controller/IndexCtrl.java
+++ src/main/java/com/example/sharefood/controller/IndexCtrl.java
@@ -18,4 +18,40 @@ public class IndexCtrl {
         }
         return "index";
     }
+
+    @GetMapping("/food")
+    public String index1(@RequestParam(value = "isAdmin",required = false) String statu,Model model){
+        if(statu == null || "unlogin".equals(statu)){
+            model.addAttribute("statu","unlogin");
+        }else if(statu.equals("yes") || statu.equals("admin")){
+            model.addAttribute("statu","admin");
+        }else if(statu.equals("no") || statu.equals("user")){
+            model.addAttribute("statu","user");
+        }
+        return "index-1";
+    }
+
+    @GetMapping("/detailfood")
+    public String index2(@RequestParam(value = "isAdmin",required = false) String statu,Model model){
+        if(statu == null || "unlogin".equals(statu)){
+            model.addAttribute("statu","unlogin");
+        }else if(statu.equals("yes") || statu.equals("admin")){
+            model.addAttribute("statu","admin");
+        }else if(statu.equals("no") || statu.equals("user")){
+            model.addAttribute("statu","user");
+        }
+        return "index-2";
+    }
+
+    @GetMapping("/foodblog")
+    public String index3(@RequestParam(value = "isAdmin",required = false) String statu,Model model){
+        if(statu == null || "unlogin".equals(statu)){
+            model.addAttribute("statu","unlogin");
+        }else if(statu.equals("yes") || statu.equals("admin")){
+            model.addAttribute("statu","admin");
+        }else if(statu.equals("no") || statu.equals("user")){
+            model.addAttribute("statu","user");
+        }
+        return "index-3";
+    }
 }
